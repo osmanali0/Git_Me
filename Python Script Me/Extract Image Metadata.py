8af589a292180a25f3ed276fcd7cccad77848d84
osmanali <osmanali0@users.noreply.github.com>
2021-06-23 16:34:12 +0200

    PIL all tags details
    اضافة نموذج من المعلومات التي يمكن استخراجها

--- Python Script Me/Extract Image Metadata.py
+++ Python Script Me/Extract Image Metadata.py
@@ -2,7 +2,12 @@
 
  # How to Extract Image Metadata in Python
  # https://www.thepythoncode.com/article/extracting-image-metadata-in-python
+ # https://github.com/python-pillow/Pillow/blob/master/src/PIL/ExifTags.py
+ #
  # 
+ #
+ #
+ #
 """
 from PIL import Image
 #import os
@@ -24,3 +29,48 @@ for tag_id in exifdata:
         data = data.decode()
     print(f"{tag:60}: {data}")
     
+    """[
+ExifVersion              : 0220
+ShutterSpeedValue        : (406, 100)
+ApertureValue            : (1851, 1000)       
+DateTimeOriginal         : 2016:11:10 18:22:47
+DateTimeDigitized        : 2016:11:10 18:22:47
+BrightnessValue          : (-109, 100)        
+ExposureBiasValue        : (0, 10)
+MaxApertureValue         : (1851, 1000)       
+MeteringMode             : 2
+Flash                    : 0
+FocalLength              : (220, 100)
+UserComment              :
+ColorSpace               : 1
+ExifImageWidth           : 2592
+FocalLengthIn35mmFilm    : 22
+SceneCaptureType         : 0
+ExifImageHeight          : 1944
+ImageWidth               : 2592
+ImageLength              : 1944
+Make                     : samsung
+Model                    : SM-G920F
+Orientation              : 8
+YCbCrPositioning         : 1
+ExposureTime             : (1, 17)
+XResolution              : (72, 1)
+YResolution              : (72, 1)
+FNumber                  : (19, 10)
+FNumber                  : (19, 10)
+ImageUniqueID            : B05LLHA01PM
+ISOSpeedRatings          : 400
+ISOSpeedRatings          : 400
+ISOSpeedRatings          : 400
+ISOSpeedRatings          : 400
+ResolutionUnit           : 2
+ExifOffset               : 226
+ExposureMode             : 0
+FlashPixVersion          : 0100
+WhiteBalance             : 0
+Software                 : G920FXXS4DPI4
+DateTime                 : 2016:11:10 18:22:4
+MakerNote                :       0100                      Z   @         P          
+                                                           Z   @         P
+    ]
+    """
